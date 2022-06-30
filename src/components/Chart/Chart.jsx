76e647a56871acb2758c0e6ebc8b93847e8c3d8c
Hoang <hoangvnl57@gmail.com>
2022-06-30 18:53:55 +0700

    enhance UI and parse response from BE

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -5,75 +5,123 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import ListItemText from "@mui/material/ListItemText";
+import Checkbox from "@mui/material/Checkbox";
 import { jsonToArray } from "./utils";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
 const PREDICTION_TYPES = {
   XGBoost: "XGBoost",
   LSTM: "LSTM",
   RNN: "RNN",
 };
 
+const PREDICTION_VALUES = {
+  CLOSE: "Close Value",
+  ROC: "Price change Value",
+};
+
 const Chart = () => {
   const [predictionType, setPredictionType] = useState(
     PREDICTION_TYPES.XGBoost
   );
+  const [predictionValue, setPredictionValue] = useState([]);
   const [isFirstLoad, setIsFirstLoad] = useState(true);
 
   const chartRef = useRef();
   const originalChart = useRef();
   const closeChart = useRef();
-  const predictCloseChart = useRef();
   const rocChart = useRef();
+  const predictCloseChart = useRef();
+  const predictROCChart = useRef();
   const ts = useRef();
+
   const [closeValueData, setCloseValueData] = useState([
     { time: 0, value: "" },
   ]);
   const [predictCloseValue, setPredictCloseValue] = useState([
     { time: 0, value: "" },
   ]);
+  const [predictROCValue, setPredictROCValue] = useState([
+    { time: 0, value: "" },
+  ]);
+
   useEffect(() => {
     originalChart.current = createChart(chartRef.current, {
-      width: 1200,
+      width: 1500,
       height: 600,
+      timeScale: {
+        timeVisible: true,
+        secondsVisible: false,
+      },
+      layout: {
+        textColor: "#d1d4dc",
+        backgroundColor: "#000000",
+      },
       rightPriceScale: {
         visible: true,
         borderColor: "rgba(197, 203, 206, 1)",
+        scaleMargins: {
+          top: 0.3,
+          bottom: 0.25,
+        },
       },
       leftPriceScale: {
         visible: true,
         borderColor: "rgba(197, 203, 206, 1)",
       },
-      layout: {
-        backgroundColor: "#ffffff",
-        textColor: "rgba(33, 56, 77, 1)",
-      },
       grid: {
-        horzLines: {
-          color: "#F0F3FA",
-        },
         vertLines: {
-          color: "#F0F3FA",
+          color: "rgba(42, 46, 57, 0)",
         },
-      },
-
-      timeScale: {
-        borderColor: "rgba(197, 203, 206, 1)",
+        horzLines: {
+          color: "rgba(42, 46, 57, 0)",
+        },
       },
       handleScroll: {
         vertTouchDrag: false,
       },
     });
-    rocChart.current = originalChart.current.addLineSeries({
+    rocChart.current = originalChart.current.addAreaSeries({
       priceScaleId: "left",
-      color: "yellow",
+      topColor: "rgba(255, 192, 0, 0.7)",
+      bottomColor: "rgba(255, 192, 0, 0.3)",
+      lineColor: "rgba(255, 192, 0, 1)",
+      lineWidth: 2,
       title: "ROC Value",
+      visible: false,
     });
-    closeChart.current = originalChart.current.addLineSeries({
+    closeChart.current = originalChart.current.addAreaSeries({
       title: "Close Value",
+      topColor: "rgba(67, 83, 254, 0.7)",
+      bottomColor: "rgba(67, 83, 254, 0.3)",
+      lineColor: "rgba(67, 83, 254, 1)",
+      lineWidth: 2,
+      visible: false,
     });
 
     predictCloseChart.current = originalChart.current.addLineSeries({
       title: "Prediction Close Value",
       color: "green",
+      visible: false,
+    });
+
+    predictROCChart.current = originalChart.current.addLineSeries({
+      title: "Prediction ROC Value",
+      priceScaleId: "left",
+      color: "red",
+      visible: false,
     });
 
     isFirstLoad &&
@@ -85,10 +133,10 @@ const Chart = () => {
         .then(() => {
           fetch(`http://127.0.0.1:5000/predict?type=${predictionType}`)
             .then((r) => r.json())
-            .then((r) => JSON.parse(r))
             .then((response) => {
               setIsFirstLoad(false);
-              setPredictCloseValue(jsonToArray(response));
+              setPredictCloseValue(jsonToArray(JSON.parse(response["Close"])));
+              setPredictROCValue(jsonToArray(JSON.parse(response["ROC"])));
             });
         });
 
@@ -101,10 +149,10 @@ const Chart = () => {
     !isFirstLoad &&
       fetch(`http://127.0.0.1:5000/predict?type=${predictionType}`)
         .then((r) => r.json())
-        .then((r) => JSON.parse(r))
         .then((response) => {
           setIsFirstLoad(false);
-          setPredictCloseValue(jsonToArray(response));
+          setPredictCloseValue(jsonToArray(JSON.parse(response["Close"])));
+          setPredictROCValue(jsonToArray(JSON.parse(response["ROC"])));
         });
   }, [predictionType]);
 
@@ -125,6 +173,7 @@ const Chart = () => {
 
   useEffect(() => {
     predictCloseChart.current.setData(predictCloseValue);
+    predictROCChart.current.setData(predictROCValue);
   }, [predictCloseValue]);
 
   useEffect(() => {
@@ -155,10 +204,13 @@ const Chart = () => {
           `http://127.0.0.1:5000/update-predict?type=${predictionType}&time=${time}&value=${candlestick.c}`
         )
           .then((r) => r.json())
-          .then((r) => JSON.parse(r))
           .then((response) => {
-            const result = jsonToArray(response);
-            predictCloseChart.current.update(result[0]);
+            predictCloseChart.current.update(
+              jsonToArray(JSON.parse(response["Close"]))[0]
+            );
+            predictROCChart.current.update(
+              jsonToArray(JSON.parse(response["ROC"]))[0]
+            );
           });
       }
 
@@ -178,29 +230,71 @@ const Chart = () => {
     predictCloseChart.current.setData([]);
   };
 
+  const handleChangePredictionValue = (e) => {
+    setPredictionValue(e.target.value);
+    if (e.target.value.indexOf("CLOSE") > -1) {
+      closeChart.current.applyOptions({ visible: true });
+      predictCloseChart.current.applyOptions({ visible: true });
+    } else {
+      closeChart.current.applyOptions({ visible: false });
+      predictCloseChart.current.applyOptions({ visible: false });
+    }
+    if (e.target.value.indexOf("ROC") > -1) {
+      rocChart.current.applyOptions({ visible: true });
+      predictROCChart.current.applyOptions({ visible: true });
+    } else {
+      rocChart.current.applyOptions({ visible: false });
+      predictROCChart.current.applyOptions({ visible: false });
+    }
+  };
+
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-      <FormControl style={{ minWidth: "200px", margin: "20px 50px" }}>
-        <InputLabel id="demo-simple-select-label">
-          Choose Prediction Type
-        </InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={predictionType}
-          label="Choose Prediction Type"
-          onChange={handleChangePredictionType}
-        >
-          {Object.keys(PREDICTION_TYPES).map((item) => (
-            <MenuItem
-              key={PREDICTION_TYPES[item]}
-              value={PREDICTION_TYPES[item]}
-            >
-              {PREDICTION_TYPES[item]}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+      <div style={{ display: "flex" }}>
+        <FormControl style={{ minWidth: "200px", margin: "20px 50px" }}>
+          <InputLabel id="demo-simple-select-label">
+            Choose Prediction Type
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={predictionType}
+            label="Choose Prediction Type"
+            onChange={handleChangePredictionType}
+          >
+            {Object.keys(PREDICTION_TYPES).map((item) => (
+              <MenuItem
+                key={PREDICTION_TYPES[item]}
+                value={PREDICTION_TYPES[item]}
+              >
+                {PREDICTION_TYPES[item]}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl style={{ minWidth: "500px", margin: "20px 50px" }}>
+          <InputLabel id="demo-multiple-checkbox-label">
+            Choose value to predict
+          </InputLabel>
+          <Select
+            labelId="demo-multiple-checkbox-label"
+            id="demo-multiple-checkbox"
+            multiple
+            value={predictionValue}
+            onChange={handleChangePredictionValue}
+            input={<OutlinedInput label="Choose value to predict" />}
+            renderValue={(selected) => selected.join(", ")}
+            MenuProps={MenuProps}
+          >
+            {Object.keys(PREDICTION_VALUES).map((item) => (
+              <MenuItem key={item} value={item}>
+                <Checkbox checked={predictionValue.indexOf(item) > -1} />
+                <ListItemText primary={PREDICTION_VALUES[item]} />
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
       <div ref={chartRef} />
     </div>
   );
--- src/components/Chart/utils.js
+++ src/components/Chart/utils.js
@@ -1,14 +1,8 @@
 const jsonToArray = (js) => {
-  console.log(js);
-  // const result = js?.Date.map((i, index) => {
-  //   return { time: i, value: js.Predictions[index] };
-  // });
-
   const result = Object.keys(js.Date).map((i) => ({
     time: js.Date[i],
     value: js.Predictions[i],
   }));
-
   return result;
 };
 
